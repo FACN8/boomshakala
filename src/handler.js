41602aa0d94a56dd172ff0a2612faf4b19e57532
Ivan Khuri <ivankhuri@gmail.com>
2020-01-21 15:17:18 +0200

    Added homepage routing

--- src/handler.js
+++ src/handler.js
@@ -2,42 +2,53 @@ const fs = require('fs');
 const path = require('path');
 const querystring = require('querystring');
 
-
-var publicHandler = (request, response) => {
-    const url = request.url;
-    const extension = url.split('.')[1];
-    const extentionType = {
-        html: 'text/html',
-        css: 'text/css',
-        js: 'application/js',
-        ico: 'image/x-icon',
-        png: 'image/png',
-        jpg: 'image/jpg',
-        json: 'application/json'
-    }
-    const filePath = path.join(__dirname, '..', url)
-
-    fs.readFile(filePath, (error, file) => {
-        if (error) {
-            console.log(error)
-            response.writeHead(500)
-            response.end('An Error Occured')
-        } else {
-            response.writeHead(200, { 'Content-Type': extensionType[extension] })
-            response.end(file)
-        }
-    })
+const extensionType = {
+    html: 'text/html',
+    css: 'text/css',
+    js: 'application/javascript',
+    ico: 'image/x-icon',
+    png: 'image/png',
+    jpg: 'image/jpg',
+    json: 'application/json'
 }
 
-var srcHandler = (request, response) => {}
+var publicHandler = (request, response) => {
+    const url = path.join(__dirname, '..', request.url);
+    const extension = url.split('.')[1];
 
-var resHandler = (request, response) => {}
+    fs.readFile(url, (error, file) => {
+        if (error) {
+            console.log(error);
+            response.writeHead(404, { 'Content-Type': extensionType.html });
+            response.end('An Error Occured');
+            return;
+        }
+        response.writeHead(200, { 'Content-Type': extensionType[extension] });
+        response.end(file);
+    });
+}
 
-var inputHandler = (request, response) => {}
+var resHandler = (request, response) => { }
+
+var inputHandler = (request, response) => { }
+
+var homePageHandler = (request, response) => {
+    var url = path.join(__dirname, '..', 'public', 'index.html');
+
+    fs.readFile(url, (error, file) => {
+        if (error) {
+            response.writeHead(404, { 'Content-Type': extensionType.html });
+            response.end('404 File not found');
+            return;
+        }
+        response.writeHead(200, { 'Content-Type': extensionType.html});
+        response.end(file);
+    });
+}
 
 module.exports = {
     publicHandler,
-    srcHandler,
     resHandler,
-    inputHandler
+    inputHandler,
+    homePageHandler
 }
